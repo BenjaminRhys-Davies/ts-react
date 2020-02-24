28082163633b8ca68880f1d39d92115846b59540
Benjamin Rhys-Davies <benjamin.rhys-davies@sonovate.com>
2020-02-24 10:28:46 +0000

    feat: simple project skeleton

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+process.env.TZ = 'GMT';
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  coverageDirectory: './coverage',
+  collectCoverageFrom: ['src/**/*.{js,ts}', '!**/node_modules/**', '!**/build/**', '!**/coverage/**'],
+  modulePaths: ['./'],
+  testMatch: ['**/*.{test,spec}.ts'],
+};
